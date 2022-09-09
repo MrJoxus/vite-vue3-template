c032c8c295df9a9ef6f14487c22ae624e2c8cc48
Joachim Kiedrowski <joachim@machenwir.so>
2022-09-10 00:38:23 +0200

    Add env to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import Pages from 'vite-plugin-pages'
@@ -6,18 +6,26 @@ import Layouts from 'vite-plugin-vue-layouts'
 import VueI18n from '@intlify/vite-plugin-vue-i18n'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    Pages(),
-    Layouts(),
-    AutoImport({
-      dts: 'src/auto-imports.d.ts',
-      imports: [
-        'vue',
-        'vue-i18n',
-      ],
-    }),
-    VueI18n(),
-  ],
+export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
+  return {
+    server: {
+      port: process.env.VITE_PORT,
+    },
+    plugins: [
+      vue(),
+      Pages(),
+      Layouts(),
+      AutoImport({
+        dts: 'src/auto-imports.d.ts',
+        imports: [
+          'vue',
+          'vue-i18n',
+        ],
+      }),
+      VueI18n(),
+    ],
+  }
 })
