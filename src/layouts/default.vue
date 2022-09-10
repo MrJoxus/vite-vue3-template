2bb2396ed48d137fe8a139de073ae7ca80fa06c2
Joachim Kiedrowski <joachim@machenwir.so>
2022-09-10 17:17:34 +0200

    Add basic index Page

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,8 +1,26 @@
+<script setup lang="ts">
+import { NConfigProvider, NLayout } from 'naive-ui'
+import { useThemeStore } from '../stores/themes'
+
+const themeStore = useThemeStore()
+
+onMounted(() => {
+  themeStore.getThemeFromLocalStore()
+})
+</script>
+
 <template>
-  <main>
-    <router-view />
-    <div>
-      [Default Layout]
-    </div>
-  </main>
+  <NConfigProvider :theme="themeStore.theme">
+    <NLayout class="main">
+      <main>
+        <router-view />
+      </main>
+    </NLayout>
+  </NConfigProvider>
 </template>
+
+<style lang="scss">
+.main{
+  min-height: 100vh;
+}
+</style>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,31 +1,77 @@
 <script setup lang="ts">
+import { NButton, NButtonGroup, NCard, NH1, NSelect, NText } from 'naive-ui'
+import { useThemeStore } from '../stores/themes'
+
+const themeStore = useThemeStore()
 const { t, locale } = useI18n()
+
+const localeOptions = ref([
+  {
+    label: 'DE',
+    value: 'de',
+  },
+  {
+    label: 'EN',
+    value: 'en',
+  },
+])
+
+const themeOptions = ref([
+  { title: 'Light', action: themeStore.setLightTheme },
+  { title: 'Dark', action: themeStore.setDarkTheme },
+  { title: 'OS', action: themeStore.setOsTheme },
+])
 </script>
 
 <template>
-  <div>
-    <form>
-      <label>{{ t('language') }} {{ $i18n.locale }}</label>
-      <select v-model="locale">
-        <option value="en">
-          en
-        </option>
-        <option value="de">
-          de
-        </option>
-      </select>
-    </form>
-    <p>{{ t('hello') }}</p>
+  <div class="banner">
+    <NH1>
+      vite-vue3-template
+    </NH1>
+    <NCard>
+      <p>
+        <NText>{{ t('theme') }}</NText>
+      </p>
+      <NButtonGroup>
+        <NButton v-for="themeOption in themeOptions" :key="themeOption.title" @click="themeOption.action">
+          {{ themeOption.title }}
+        </NButton>
+      </NButtonGroup>
+      <div>
+        <p>
+          <NText>{{ t('choseLanguage') }}</NText>
+        </p>
+        <NSelect v-model:value="locale" :options="localeOptions" />
+        <p>
+          <NText>{{ t('hello') }}</NText>
+        </p>
+      </div>
+    </NCard>
   </div>
 </template>
+
+<style lang="scss">
+  .banner{
+    max-width: 600px;
+    margin: 0 auto;
+    display: flex;
+    flex-direction: column;
+    height: calc(100vh - 64px);
+    justify-content: center;
+    position: relative;
+    text-align: center;
+  }
+  </style>
 
 <i18n lang="yaml">
 de:
   locale: Deutsch
-  language: Sprache
+  choseLanguage: 'Sprache wählen:'
   hello: 'Hallo, Welt!'
+  theme: Theme
 en:
   locale: English
-  language: Language
+  choseLanguage: 'Chose Language:'
   hello: 'hello, world!'
-</i18n>
+  theme: Theme
+  </i18n>
